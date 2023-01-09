f75236deeb27a5d8a0a75d221b67c01ccd55ec3b
joeylichang <joeycli0919@gmail.com>
2023-01-09 13:25:40 +0800

    feat: adjust entry func code

--- cmd/storage_provider/main.go
+++ cmd/storage_provider/main.go
@@ -31,6 +31,53 @@ var (
 	SyncerService    string = "Syncer"
 )
 
+// initService init service instance by name and config.
+func initService(serviceName string, cfg *config.StorageProviderConfig) (server lifecycle.Service, err error) {
+	switch serviceName {
+	case GetaWayService:
+		if cfg.GatewayCfg == nil {
+			cfg.GatewayCfg = config.DefaultStorageProviderConfig.GatewayCfg
+		}
+		server, err = gateway.NewGatewayService(cfg.GatewayCfg)
+		if err != nil {
+			return nil, err
+		}
+	case UploaderService:
+		if cfg.UploaderCfg == nil {
+			cfg.UploaderCfg = config.DefaultStorageProviderConfig.UploaderCfg
+		}
+		server, err = uploader.NewUploaderService(cfg.UploaderCfg)
+		if err != nil {
+			return nil, err
+		}
+	case StoneHubService:
+		if cfg.StoneHubCfg == nil {
+			cfg.StoneHubCfg = config.DefaultStorageProviderConfig.StoneHubCfg
+		}
+		server, err = stonehub.NewStoneHubService(cfg.StoneHubCfg)
+		if err != nil {
+			return nil, err
+		}
+	case StoneNodeService:
+		if cfg.StoneNodeCfg == nil {
+			cfg.StoneNodeCfg = config.DefaultStorageProviderConfig.StoneNodeCfg
+		}
+		server, err = stonenode.NewStoneNodeService(cfg.StoneNodeCfg)
+		if err != nil {
+			return nil, err
+		}
+	case SyncerService:
+		if cfg.SyncerCfg == nil {
+			cfg.SyncerCfg = config.DefaultStorageProviderConfig.SyncerCfg
+		}
+		server, err = syncer.NewSyncerService(cfg.SyncerCfg)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return server, nil
+}
+
 func main() {
 	flag.Parse()
 	if *version {
@@ -38,67 +85,19 @@ func main() {
 		os.Exit(0)
 	}
 
-	var (
-		ctx = context.Background()
-		cfg = config.LoadConfig(*configFile)
-		slc = lifecycle.NewServiceLifecycle()
-	)
+	cfg := config.LoadConfig(*configFile)
+	slc := lifecycle.NewServiceLifecycle()
 	for _, serviceName := range cfg.Service {
-		switch serviceName {
-		case GetaWayService:
-			if cfg.GatewayCfg == nil {
-				cfg.GatewayCfg = config.DefaultStorageProviderConfig.GatewayCfg
-			}
-			server, err := gateway.NewGatewayService(cfg.GatewayCfg)
-			if err != nil {
-				log.Errorw("gateway init failed", "error", err)
-				os.Exit(1)
-			}
-			slc.RegisterServices(server)
-		case UploaderService:
-			if cfg.UploaderCfg == nil {
-				cfg.UploaderCfg = config.DefaultStorageProviderConfig.UploaderCfg
-			}
-			server, err := uploader.NewUploaderService(cfg.UploaderCfg)
-			if err != nil {
-				log.Errorw("uploader init failed", "error", err)
-				os.Exit(1)
-			}
-			slc.RegisterServices(server)
-		case StoneHubService:
-			if cfg.StoneHubCfg == nil {
-				cfg.StoneHubCfg = config.DefaultStorageProviderConfig.StoneHubCfg
-			}
-			server, err := stonehub.NewStoneHubService(cfg.StoneHubCfg)
-			if err != nil {
-				log.Errorw("stone hub init failed", "error", err)
-				os.Exit(1)
-			}
-			slc.RegisterServices(server)
-		case StoneNodeService:
-			if cfg.StoneNodeCfg == nil {
-				cfg.StoneNodeCfg = config.DefaultStorageProviderConfig.StoneNodeCfg
-			}
-			server, err := stonenode.NewStoneNodeService(cfg.StoneNodeCfg)
-			if err != nil {
-				log.Errorw("stone node init failed", "error", err)
-				os.Exit(1)
-			}
-			slc.RegisterServices(server)
-		case SyncerService:
-			if cfg.SyncerCfg == nil {
-				cfg.SyncerCfg = config.DefaultStorageProviderConfig.SyncerCfg
-			}
-			server, err := syncer.NewSyncerService(cfg.SyncerCfg)
-			if err != nil {
-				log.Errorw("syncer init failed", "error", err)
-				os.Exit(1)
-			}
-			slc.RegisterServices(server)
+		// 1. init service instance.
+		service, err := initService(serviceName, cfg)
+		if err != nil {
+			log.Errorw("init service failed ", "error", err)
 		}
-		log.Info("init service success ", serviceName)
-
+		log.Infow("init service success", "service", serviceName)
+		// 2. register service to lifecycle.
+		slc.RegisterServices(service)
 	}
-	// start all services, and listen signals
+	// 3. start all services and listen os signals.
+	ctx := context.Background()
 	slc.Signals(syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP).StartServices(ctx).Wait(ctx)
 }
