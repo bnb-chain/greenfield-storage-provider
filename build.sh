caaebf2c3ea8e81c4aab529c4e2ff14fa9355610
dylanhuang <j75689@gmail.com>
2023-02-06 16:47:08 +0800

    ci: add docker release pipe (#74)

--- build.sh
+++ build.sh
@@ -1,26 +1,23 @@
 #!/usr/bin/env bash
-Version=`git describe --abbrev=0 --tags`
+Version=`git describe --abbrev=0 --tags --always`
 BranchName=`git rev-parse --abbrev-ref HEAD`
 CommitID=`git rev-parse HEAD`
 BuildTime=`date +%Y-%m-%d\ %H:%M`
 
 #[[ "-$GOPATH" == "-" ]] && { echo "GOPATH not set"; exit 1; }
 
-buf generate
-
-go build -ldflags "\
--X 'main.Version=${Version}' \
--X 'main.CommitID=${CommitID}' \
--X 'main.BranchName=${BranchName}' \
--X 'main.BuildTime=${BuildTime}'" \
--o storage_provider cmd/storage_provider/*.go
-
-go build -o test-storage-provider test/e2e/services/case_driver.go
-
 if [ ! -d build  ];then
-  mkdir build
+  mkdir -p build
   mkdir -p build/data
 fi
-mv storage_provider ./build
-mv test-storage-provider ./build
-cp config/config.toml ./build
+
+go build -ldflags "\
+  -X 'main.Version=${Version}' \
+  -X 'main.CommitID=${CommitID}' \
+  -X 'main.BranchName=${BranchName}' \
+  -X 'main.BuildTime=${BuildTime}'" \
+-o ./build/storage_provider cmd/storage_provider/*.go
+
+go build -o ./build/test-storage-provider test/e2e/services/case_driver.go
+
+cp config/config.toml ./build/
