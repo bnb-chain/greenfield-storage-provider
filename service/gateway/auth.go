68c1d97c8b72601f0ae4a856446c6293073269eb
will-2012 <117156346+will-2012@users.noreply.github.com>
2023-02-09 09:14:54 +0800

    feat(gateway): verify signature (#65)

--- service/gateway/auth.go
+++ service/gateway/auth.go
@@ -1,7 +1,7 @@
 package gateway
 
 // todo: impl
-func (rc *requestContext) verifyAuth(client *retrieverClient) error {
+func (requestContext *requestContext) verifyAuth(client *retrieverClient) error {
 	// check account/bucket by retriever
 	return nil
 }
--- service/gateway/sign.go
+++
@@ -1,7 +0,0 @@
-package gateway
-
-// todo
-func (rc *requestContext) verifySign() error {
-	// todo: set account to context
-	return nil
-}
