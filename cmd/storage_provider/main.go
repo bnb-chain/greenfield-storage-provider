73d3df34782663c4e5d27ff3f7a46d316fbe55a5
joeylichang <joeycli0919@gmail.com>
2023-03-03 00:36:56 +0800

    feat: add command for devops andconfig log

--- cmd/storage_provider/main.go
+++ cmd/storage_provider/main.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strings"
 	"syscall"
 
-	"gopkg.in/urfave/cli.v1"
+	"github.com/bnb-chain/greenfield-storage-provider/model"
+	"github.com/urfave/cli/v2"
 
 	"github.com/bnb-chain/greenfield-storage-provider/cmd/conf"
 	"github.com/bnb-chain/greenfield-storage-provider/cmd/utils"
@@ -27,8 +29,13 @@ var (
 	spFlags = []cli.Flag{
 		utils.VersionFlag,
 		utils.ConfigFileFlag,
+		utils.ConfigRemoteFlag,
+		utils.ServerFlag,
 		utils.DBUserFlag,
-		utils.DBUserFlag,
+		utils.DBPasswordFlag,
+		utils.DBAddressFlag,
+		utils.LogLevelFlag,
+		utils.LogPathFlag,
 	}
 )
 
@@ -39,10 +46,12 @@ func init() {
 	app.Action = storageProvider
 	app.HideVersion = true
 	app.Flags = append(app.Flags, spFlags...)
-	app.Commands = []cli.Command{
+	app.Commands = []*cli.Command{
 		// config category commands
 		conf.ConfigDumpCmd,
 		conf.ConfigUploadCmd,
+		// miscellaneous commands
+		utils.ListServiceCmd,
 	}
 }
 
@@ -53,16 +62,55 @@ func main() {
 	}
 }
 
+// makeConfig loads the configuration and creates the storage provider backend.
+func makeConfig(ctx *cli.Context) (*config.StorageProviderConfig, error) {
+	cfg := &config.StorageProviderConfig{}
+	var services []string
+	if ctx.IsSet(utils.ServerFlag.Name) {
+		services = utils.SplitTagsFlag(ctx.String(utils.ServerFlag.Name))
+		for _, service := range services {
+			if _, ok := model.SpServiceDesc[strings.ToLower(service)]; !ok {
+				return nil, fmt.Errorf("invalid service %s", service)
+			}
+		}
+	}
+	if ctx.IsSet(utils.ConfigRemoteFlag.Name) {
+		//&storeconf.SQLDBConfig{
+		//	User:     ctx.String(utils.DBUserFlag.Name),
+		//	Passwd:   ctx.String(utils.DBPasswordFlag.Name),
+		//	Address:  ctx.String(utils.DBAddressFlag.Name),
+		//	Database: "job_db",
+		//}
+		// TODO:: new sp db load config by services
+	} else if ctx.IsSet(utils.ConfigFileFlag.Name) {
+		cfg = config.LoadConfig(ctx.String(utils.ConfigFileFlag.Name))
+	} else {
+		cfg = config.DefaultStorageProviderConfig
+	}
+	if ctx.IsSet(utils.ServerFlag.Name) {
+		cfg.Service = services
+	}
+	return cfg, nil
+}
+
+// storageProvider is the main entry point into the system if no special subcommand is ran.
+// It uses default config to run storage provider services based on the command line arguments
+// and runs it in blocking mode, waiting for it to be shut down.
 func storageProvider(ctx *cli.Context) error {
-	if ctx.GlobalIsSet(utils.VersionFlag.Name) {
-		fmt.Print(DumpLogo() + DumpVersion())
+	if ctx.IsSet(utils.VersionFlag.Name) {
+		fmt.Print(DumpLogo() + "\n" + DumpVersion())
 		return nil
 	}
-	if !ctx.GlobalIsSet(utils.ConfigFileFlag.Name) {
-		return fmt.Errorf("invalid params")
+	logLevel, err := log.ParseLevel(ctx.String(utils.LogLevelFlag.Name))
+	if err != nil {
+		return err
 	}
+	log.Init(logLevel, ctx.String(utils.LogPathFlag.Name))
 
-	cfg := config.LoadConfig(ctx.GlobalString(utils.ConfigFileFlag.Name))
+	cfg, err := makeConfig(ctx)
+	if err != nil {
+		return err
+	}
 	slc := lifecycle.NewServiceLifecycle()
 	for _, serviceName := range cfg.Service {
 		// 1. init service instance.
