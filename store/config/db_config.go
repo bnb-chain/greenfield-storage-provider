b97a0410de2f3d810e5a1e3a97fea45b37faced2
joeylichang <joeycli0919@gmail.com>
2023-04-05 20:26:15 +0800

    fix: add sql db connection config

--- store/config/db_config.go
+++ store/config/db_config.go
@@ -2,8 +2,12 @@ package config
 
 // SQLDBConfig is sql db config
 type SQLDBConfig struct {
-	User     string
-	Passwd   string
-	Address  string
-	Database string
+	User            string
+	Passwd          string
+	Address         string
+	Database        string
+	ConnMaxLifetime int
+	ConnMaxIdleTime int
+	MaxIdleConns    int
+	MaxOpenConns    int
 }
