b66a2dbf5664f5dfc9ce5ed2d4d21f036b431e2e
will-2012 <xibaow2020@qq.com>
2023-04-19 22:14:06 +0800

    test: add ut

--- service/gateway/request_util.go
+++ service/gateway/request_util.go
@@ -25,30 +25,32 @@ import (
 
 // requestContext is a request context.
 type requestContext struct {
+	request     *http.Request
+	routerName  string
 	requestID   string
 	bucketName  string
 	objectName  string
-	request     *http.Request
-	startTime   time.Time
+	accountID   string // accountID is used to provide authentication to the sp
 	vars        map[string]string
+	startTime   time.Time
 	skipAuth    bool // TODO: for auth v2 test, remove it in the future
+	isAnonymous bool // It is anonymous when there is no GnfdAuthorizationHeader
 	bucketInfo  *storagetypes.BucketInfo
 	objectInfo  *storagetypes.ObjectInfo
-	accountID   string // accountID is used to provide authentication to the sp
-	isAnonymous bool
 }
 
 // newRequestContext return a request context.
 func newRequestContext(r *http.Request) *requestContext {
 	vars := mux.Vars(r)
 	return &requestContext{
+		request:    r,
+		routerName: mux.CurrentRoute(r).GetName(),
 		requestID:  util.GenerateRequestID(),
 		bucketName: vars["bucket"],
 		objectName: vars["object"],
 		accountID:  vars["account_id"],
-		request:    r,
-		startTime:  time.Now(),
 		vars:       vars,
+		startTime:  time.Now(),
 	}
 }
 
@@ -103,7 +105,7 @@ func (reqContext *requestContext) verifySignature() (sdk.AccAddress, error) {
 		return reqContext.verifySignatureV2(requestSignature[len(v2SignaturePrefix):])
 	}
 	// Anonymous users can get public object.
-	if requestSignature == "" && mux.CurrentRoute(reqContext.request).GetName() == getObjectRouterName {
+	if requestSignature == "" && reqContext.routerName == getObjectRouterName {
 		reqContext.isAnonymous = true
 		return sdk.AccAddress{}, nil
 	}
@@ -256,7 +258,7 @@ func (g *Gateway) checkAuthorization(reqContext *requestContext, addr sdk.AccAdd
 		}
 	}
 
-	switch mux.CurrentRoute(reqContext.request).GetName() {
+	switch reqContext.routerName {
 	case putObjectRouterName:
 		if reqContext.bucketInfo, reqContext.objectInfo, err = g.chain.QueryBucketInfoAndObjectInfo(
 			context.Background(), reqContext.bucketName, reqContext.objectName); err != nil {
