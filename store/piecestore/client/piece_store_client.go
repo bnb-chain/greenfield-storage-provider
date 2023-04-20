13a2119b6697e52b0acc06e65128118894242e58
VM <arimas@foxmail.com>
2023-04-20 18:45:34 +0800

    fix: add spdb and piece store metrics for downloader

--- store/piecestore/client/piece_store_client.go
+++ store/piecestore/client/piece_store_client.go
@@ -4,8 +4,10 @@ import (
 	"bytes"
 	"context"
 	"io"
+	"time"
 
 	"github.com/bnb-chain/greenfield-storage-provider/pkg/log"
+	"github.com/bnb-chain/greenfield-storage-provider/pkg/metrics"
 	"github.com/bnb-chain/greenfield-storage-provider/store/piecestore/piece"
 	"github.com/bnb-chain/greenfield-storage-provider/store/piecestore/storage"
 )
@@ -23,6 +25,11 @@ type StoreClient struct {
 	ps *piece.PieceStore
 }
 
+const (
+	getPieceMethodName = "getPiece"
+	putPieceMethodName = "putPiece"
+)
+
 func NewStoreClient(pieceConfig *storage.PieceStoreConfig) (*StoreClient, error) {
 	ps, err := piece.NewPieceStore(pieceConfig)
 	if err != nil {
@@ -31,21 +38,36 @@ func NewStoreClient(pieceConfig *storage.PieceStoreConfig) (*StoreClient, error)
 	return &StoreClient{ps: ps}, nil
 }
 
+// GetPiece gets piece data from piece store
 func (client *StoreClient) GetPiece(ctx context.Context, key string, offset, limit int64) ([]byte, error) {
+	startTime := time.Now()
+	defer func() {
+		observer := metrics.PieceStoreTimeHistogram.WithLabelValues(getPieceMethodName)
+		observer.Observe(time.Since(startTime).Seconds())
+		metrics.PieceStoreRequestTotal.WithLabelValues(getPieceMethodName).Inc()
+	}()
+
 	rc, err := client.ps.Get(ctx, key, offset, limit)
 	if err != nil {
-		log.Errorw("get piece data from piece store failed", "error", err)
+		log.Errorw("failed to get piece data from piece store", "error", err)
 		return nil, err
 	}
 	buf := &bytes.Buffer{}
 	_, err = io.Copy(buf, rc)
 	if err != nil {
-		log.Errorw("copy data failed", "error", err)
+		log.Errorw("failed to copy data", "error", err)
 		return nil, err
 	}
 	return buf.Bytes(), nil
 }
 
+// PutPiece puts piece to piece store
 func (client *StoreClient) PutPiece(key string, value []byte) error {
+	startTime := time.Now()
+	defer func() {
+		observer := metrics.PieceStoreTimeHistogram.WithLabelValues(putPieceMethodName)
+		observer.Observe(time.Since(startTime).Seconds())
+		metrics.PieceStoreRequestTotal.WithLabelValues(putPieceMethodName).Inc()
+	}()
 	return client.ps.Put(context.Background(), key, bytes.NewReader(value))
 }
