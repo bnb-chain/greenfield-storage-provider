cfc8adc16551008f911fab7ac151324d3f279334
annielz <lmlmzhen@sina.com>
2023-04-26 16:16:44 +0800

    fix: put header in right place

--- service/gateway/object_handler.go
+++ service/gateway/object_handler.go
@@ -336,10 +336,8 @@ func (gateway *Gateway) getObjectByUniversalEndpointHandler(w http.ResponseWrite
 
 		if isDownload {
 			redirectUrl = endpoint + "/download/" + reqContext.bucketName + "/" + reqContext.objectName
-			w.Header().Set(model.ContentDispositionHeader, model.ContentDispositionAttachmentValue+"; filename=\""+escapedObjectName+"\"")
 		} else {
 			redirectUrl = endpoint + "/view/" + reqContext.bucketName + "/" + reqContext.objectName
-			w.Header().Set(model.ContentDispositionHeader, model.ContentDispositionInlineValue)
 		}
 
 		log.Debugw("getting redirect url:", "redirectUrl", redirectUrl)
@@ -383,6 +381,12 @@ func (gateway *Gateway) getObjectByUniversalEndpointHandler(w http.ResponseWrite
 		log.Errorf("failed to get object", "error", err)
 		errDescription = makeErrorDescription(err)
 		return
+	}
+
+	if isDownload {
+		w.Header().Set(model.ContentDispositionHeader, model.ContentDispositionAttachmentValue+"; filename=\""+escapedObjectName+"\"")
+	} else {
+		w.Header().Set(model.ContentDispositionHeader, model.ContentDispositionInlineValue)
 	}
 
 	for {
