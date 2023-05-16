39d40deb4bd05f3179feb12bf6fc844bd1bf1850
actioncli <joeycli0919@qq.com>
2023-05-16 22:33:03 +0800

    chore: delete old metrice items

--- pkg/metrics/metric_items.go
+++ pkg/metrics/metric_items.go
@@ -46,9 +46,7 @@ var MetricsItems = []prometheus.Collector{
 	DispatchSealObjectTaskCounter,
 	DispatchReceivePieceTaskCounter,
 	DispatchGcObjectTaskCounter,
-
 	SealObjectTimeHistogram,
-
 	BlockHeightLagGauge,
 }
 
@@ -265,34 +263,6 @@ var (
 		Name: "block_syncer_height",
 		Help: "Current block number of block syncer progress.",
 	}, []string{"service"})
-
-	// SealObjectTimeHistogram records sealing object time of task node service
-	//SealObjectTimeHistogram = prometheus.NewHistogramVec(prometheus.HistogramOpts{
-	//	Name:    "task_node_seal_object_time",
-	//	Help:    "Track task node service the time of sealing object on chain.",
-	//	Buckets: prometheus.DefBuckets,
-	//}, []string{serviceLabelName})
-	// SealObjectTotalCounter records total seal object number
-	//SealObjectTotalCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
-	//	Name: "task_node_seal_object_total",
-	//	Help: "Track task node service handles total seal object number",
-	//}, []string{"success_or_failure"})
-	//// ReplicateObjectTaskGauge records total replicate object number
-	//ReplicateObjectTaskGauge = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-	//	Name: "task_node_replicate_object_task_number",
-	//	Help: "Track task node service replicate object task",
-	//}, []string{serviceLabelName})
-	//// PieceStoreTimeHistogram records piece store request time
-	//PieceStoreTimeHistogram = prometheus.NewHistogramVec(prometheus.HistogramOpts{
-	//	Name:    "piece_store_handling_seconds",
-	//	Help:    "Track the latency for piece store requests",
-	//	Buckets: prometheus.DefBuckets,
-	//}, []string{"method_name"})
-	//// PieceStoreRequestTotal records piece store total request
-	//PieceStoreRequestTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
-	//	Name: "piece_store_total_requests",
-	//	Help: "Track piece store handles total request",
-	//}, []string{"method_name"})
 	SPDBTimeHistogram = prometheus.NewHistogramVec(prometheus.HistogramOpts{
 		Name:    "sp_db_handling_seconds",
 		Help:    "Track the latency for spdb requests",
