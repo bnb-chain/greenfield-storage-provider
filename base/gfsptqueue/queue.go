6c0b9fd6985b72e4607ea9aba41aaaea87111d4e
joeycli <joeycli0919@gmail.com>
2023-05-24 17:35:44 +0800

    fix: task queue gc delay when call has method (#464)

--- base/gfsptqueue/queue.go
+++ base/gfsptqueue/queue.go
@@ -50,48 +50,90 @@ func (t *GfSpTQueue) Cap() int {
 func (t *GfSpTQueue) Has(key coretask.TKey) bool {
 	t.mux.RLock()
 	defer t.mux.RUnlock()
-	_, ok := t.indexer[key]
+	idx, ok := t.indexer[key]
+	if ok {
+		if idx >= len(t.tasks) {
+			log.Errorw("[BUG] index out of bounds", "queue", t.name,
+				"len", len(t.tasks), "index", idx, "key", key)
+			return false
+		}
+		task := t.tasks[idx]
+		if t.gcFunc != nil {
+			if t.gcFunc(task) {
+				t.delete(task)
+				return false
+			}
+		}
+	}
 	return ok
 }
 
 // Top returns the top task in the queue, if the queue empty, returns nil.
 func (t *GfSpTQueue) Top() coretask.Task {
+	var gcTasks []coretask.Task
 	t.mux.RLock()
-	defer t.mux.RUnlock()
+	defer func() {
+		defer t.mux.RUnlock()
+		for _, task := range gcTasks {
+			t.delete(task)
+		}
+	}()
+
 	if len(t.tasks) == 0 {
 		return nil
 	}
-	if t.filterFunc != nil {
-		for _, task := range t.tasks {
-			if t.filterFunc(task) {
-				return task
+	for i := len(t.tasks) - 1; i >= 0; i-- {
+		if t.gcFunc != nil {
+			if t.gcFunc(t.tasks[i]) {
+				gcTasks = append(gcTasks, t.tasks[i])
 			}
 		}
-		return nil
+		if t.filterFunc != nil {
+			if t.filterFunc(t.tasks[i]) {
+				return t.tasks[i]
+			}
+		} else {
+			return t.tasks[i]
+		}
 	}
-	return t.tasks[len(t.tasks)-1]
+	return nil
 }
 
 // Pop pops and returns the top task in queue, if the queue empty, returns nil.
 func (t *GfSpTQueue) Pop() coretask.Task {
-	t.mux.Lock()
-	defer t.mux.Unlock()
+	var gcTasks []coretask.Task
+	var popTask coretask.Task
+	t.mux.RLock()
+	defer func() {
+		defer t.mux.RUnlock()
+		for _, task := range gcTasks {
+			t.delete(task)
+		}
+		if popTask != nil {
+			t.delete(popTask)
+		}
+	}()
+
 	if len(t.tasks) == 0 {
 		return nil
 	}
-	if t.filterFunc != nil {
-		for i := len(t.tasks) - 1; i >= 0; i-- {
-			task := t.tasks[i]
-			if t.filterFunc(task) {
-				t.delete(task)
-				return task
+	for i := len(t.tasks) - 1; i >= 0; i-- {
+		if t.gcFunc != nil {
+			if t.gcFunc(t.tasks[i]) {
+				gcTasks = append(gcTasks, t.tasks[i])
 			}
 		}
-		return nil
+		if t.filterFunc != nil {
+			if t.filterFunc(t.tasks[i]) {
+				popTask = t.tasks[i]
+				return popTask
+			}
+		} else {
+			popTask = t.tasks[i]
+			return popTask
+		}
 	}
-	task := t.tasks[len(t.tasks)-1]
-	t.delete(task)
-	return task
+	return nil
 }
 
 // PopByKey pops the task by the task key, if the task does not exist , returns nil.
@@ -121,11 +163,12 @@ func (t *GfSpTQueue) Push(task coretask.Task) error {
 		return errors.New("repeated task")
 	}
 	if t.exceed() {
+		var gcTasks []coretask.Task
 		clear := false
 		if t.gcFunc != nil {
 			for _, backup := range t.tasks {
 				if t.gcFunc(backup) {
-					t.delete(task)
+					gcTasks = append(gcTasks, backup)
 					clear = true
 				}
 			}
@@ -133,6 +176,10 @@ func (t *GfSpTQueue) Push(task coretask.Task) error {
 		if !clear {
 			log.Warnw("queue exceed", "queue", t.name, "cap", t.cap, "len", len(t.tasks))
 			return errors.New("queue exceed")
+		} else {
+			for _, gcTask := range gcTasks {
+				t.delete(gcTask)
+			}
 		}
 	}
 	t.add(task)
