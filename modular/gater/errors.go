db8f9bbfe759504ce4c12beab07538f95f40cc15
flywukong <2229306838@qq.com>
2023-06-26 16:42:24 +0800

    fix: fix errcode

--- modular/gater/errors.go
+++ modular/gater/errors.go
@@ -45,7 +45,7 @@ var (
 	ErrConsensus = gfsperrors.Register(module.GateModularName, http.StatusBadRequest, 55001, "server slipped away, try again later")
 
 	ErrApprovalExpired        = gfsperrors.Register(module.GateModularName, http.StatusBadRequest, 550015, "approval expired")
-	ErrRecoverySP             = gfsperrors.Register(module.GateModularName, http.StatusBadRequest, 50026, "The SP is not the correct SP to recovery")
+	ErrRecoverySP             = gfsperrors.Register(module.GateModularName, http.StatusBadRequest, 50030, "The SP is not the correct SP to recovery")
 	ErrRecoveryRedundancyType = gfsperrors.Register(module.GateModularName, http.StatusBadRequest, 50027, "The redundancy type of the recovering piece is not EC")
 	ErrRecoveryTimeout        = gfsperrors.Register(module.GateModularName, http.StatusInternalServerError, 50028, "System busy, try to request later")
 )
