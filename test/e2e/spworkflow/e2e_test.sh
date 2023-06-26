d73ad9a484ec5f67b5445e0bfdc2aeb25fad14e4
randyahx <randyahx@gmail.com>
2023-06-26 18:47:08 +0800

    Update e2e_test.sh

--- test/e2e/spworkflow/e2e_test.sh
+++ test/e2e/spworkflow/e2e_test.sh
@@ -3,7 +3,7 @@
 workspace=${GITHUB_WORKSPACE}
 
 # some constants
-GREENFIELD_REPO_TAG="v0.2.3-alpha.1"
+GREENFIELD_REPO_TAG="v0.2.2-alpha.2"
 GREENFIELD_CMD_BRANCH="dev-new-version"
 MYSQL_USER="root"
 MYSQL_PASSWORD="root"
