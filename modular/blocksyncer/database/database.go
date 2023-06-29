1c81a65e6fde595494f657db6664f5a959a92894
annielz <lmlmzhen@sina.com>
2023-06-29 13:37:04 +0800

    feat: object table sharding without ListObjectsByObjectID

--- modular/blocksyncer/database/database.go
+++ modular/blocksyncer/database/database.go
@@ -1,14 +1,18 @@
 package database
 
 import (
+	"context"
 	"database/sql"
 	"errors"
 	"fmt"
-
+	"github.com/bnb-chain/greenfield-storage-provider/store/bsdb"
 	"github.com/forbole/juno/v4/database"
 	"github.com/forbole/juno/v4/database/mysql"
 	"github.com/forbole/juno/v4/database/sqlclient"
 	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+
+	"github.com/forbole/juno/v4/log"
 )
 
 var _ database.Database = &DB{}
@@ -47,4 +51,57 @@ func Cast(db database.Database) *DB {
 // errIsNotFound check if the error is not found
 func errIsNotFound(err error) bool {
 	return errors.Is(err, sql.ErrNoRows) || errors.Is(err, gorm.ErrRecordNotFound)
+}
+
+func (db *DB) AutoMigrate(ctx context.Context, tables []schema.Tabler) error {
+	q := db.Db.WithContext(ctx)
+	m := db.Db.Migrator()
+	for _, t := range tables {
+		if t.TableName() == bsdb.PrefixTreeTableName || t.TableName() == bsdb.ObjectTableName {
+			for i := 0; i < 64; i++ {
+				shardTableName := fmt.Sprintf(t.TableName()+"_%02d", i)
+				if err := q.Table(shardTableName).AutoMigrate(t); err != nil {
+					log.Errorw("migrate table failed", "table", t.TableName(), "err", err)
+					return err
+				}
+			}
+		} else {
+			if err := m.AutoMigrate(t); err != nil {
+				log.Errorw("migrate table failed", "table", t.TableName(), "err", err)
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (db *DB) PrepareTables(ctx context.Context, tables []schema.Tabler) error {
+	q := db.Db.WithContext(ctx)
+	m := db.Db.Migrator()
+
+	for _, t := range tables {
+		if t.TableName() == bsdb.PrefixTreeTableName || t.TableName() == bsdb.ObjectTableName {
+			for i := 0; i < 64; i++ {
+				shardTableName := fmt.Sprintf(t.TableName()+"_%02d", i)
+				if m.HasTable(shardTableName) {
+					continue
+				}
+				if err := q.Table(shardTableName).AutoMigrate(t); err != nil {
+					log.Errorw("migrate table failed", "table", shardTableName, "err", err)
+					return err
+				}
+			}
+		} else {
+			if m.HasTable(t.TableName()) {
+				continue
+			}
+			if err := q.Table(t.TableName()).AutoMigrate(t); err != nil {
+				log.Errorw("migrate table failed", "table", t.TableName(), "err", err)
+				return err
+			}
+		}
+
+	}
+
+	return nil
 }
--- store/bsdb/object.go
+++ store/bsdb/object.go
@@ -1,9 +1,14 @@
 package bsdb
 
 import (
+	"fmt"
 	"github.com/forbole/juno/v4/common"
+	"github.com/spaolacci/murmur3"
 	"gorm.io/gorm"
+	"sort"
 )
+
+const ObjectsNumberOfShards = 64
 
 // ListObjectsByBucketName lists objects information by a bucket name.
 // The function takes the following parameters:
@@ -44,7 +49,7 @@ func (b *BsDBImpl) ListObjectsByBucketName(bucketName, continuationToken, prefix
 		}
 
 		if includeRemoved {
-			err = b.db.Table((&Object{}).TableName()).
+			err = b.db.Scopes(ReadObjectsTable(bucketName)).
 				Select("*").
 				Where("bucket_name = ?", bucketName).
 				Scopes(filters...).
@@ -52,7 +57,7 @@ func (b *BsDBImpl) ListObjectsByBucketName(bucketName, continuationToken, prefix
 				Order("object_name asc").
 				Find(&results).Error
 		} else {
-			err = b.db.Table((&Object{}).TableName()).
+			err = b.db.Scopes(ReadObjectsTable(bucketName)).
 				Select("*").
 				Where("bucket_name = ? and removed = false", bucketName).
 				Scopes(filters...).
@@ -64,32 +69,62 @@ func (b *BsDBImpl) ListObjectsByBucketName(bucketName, continuationToken, prefix
 	return results, err
 }
 
+type ByUpdateAtAndID []*Object
+
+func (a ByUpdateAtAndID) Len() int { return len(a) }
+func (a ByUpdateAtAndID) Less(i, j int) bool {
+	if a[i].UpdateAt == a[j].UpdateAt {
+		return a[i].ID < a[j].ID
+	}
+	return a[i].UpdateAt < a[j].UpdateAt
+}
+func (a ByUpdateAtAndID) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+
 // ListDeletedObjectsByBlockNumberRange list deleted objects info by a block number range
 func (b *BsDBImpl) ListDeletedObjectsByBlockNumberRange(startBlockNumber int64, endBlockNumber int64, includePrivate bool) ([]*Object, error) {
 	var (
-		objects []*Object
-		err     error
+		totalObjects []*Object
+		objects      []*Object
+		err          error
 	)
 
 	if includePrivate {
-		err = b.db.Table((&Object{}).TableName()).
-			Select("*").
-			Where("update_at >= ? and update_at <= ? and removed = ?", startBlockNumber, endBlockNumber, true).
-			Limit(DeletedObjectsDefaultSize).
-			Order("update_at,object_id asc").
-			Find(&objects).Error
-		return objects, err
+		for i := 0; i < ObjectsNumberOfShards; i++ {
+			err = b.db.Table(GetObjectsTableNameByShardNumber(i)).
+				Select("*").
+				Where("update_at >= ? and update_at <= ? and removed = ?", startBlockNumber, endBlockNumber, true).
+				Limit(DeletedObjectsDefaultSize).
+				Order("update_at,object_id asc").
+				Find(&objects).Error
+			totalObjects = append(totalObjects, objects...)
+		}
+	} else {
+		for i := 0; i < ObjectsNumberOfShards; i++ {
+			objectTableName := GetObjectsTableNameByShardNumber(i)
+			joins := fmt.Sprintf("right join buckets on buckets.bucket_id = %s.bucket_id", objectTableName)
+			order := fmt.Sprintf("%s.update_at, %s.object_id asc", objectTableName, objectTableName)
+			where := fmt.Sprintf("%s.update_at >= ? and %s.update_at <= ? and %s.removed = ? and "+
+				"((%s.visibility='VISIBILITY_TYPE_PUBLIC_READ') or "+
+				"(%s.visibility='VISIBILITY_TYPE_INHERIT' and buckets.visibility='VISIBILITY_TYPE_PUBLIC_READ'))",
+				objectTableName, objectTableName, objectTableName, objectTableName, objectTableName)
+
+			err = b.db.Table(objectTableName).
+				Select(objectTableName+".*").
+				Joins(joins).
+				Where(where, startBlockNumber, endBlockNumber, true).
+				Limit(DeletedObjectsDefaultSize).
+				Order(order).
+				Find(&objects).Error
+			totalObjects = append(totalObjects, objects...)
+		}
 	}
-	err = b.db.Table((&Bucket{}).TableName()).
-		Select("objects.*").
-		Joins("left join objects on buckets.bucket_id = objects.bucket_id").
-		Where("objects.update_at >= ? and objects.update_at <= ? and objects.removed = ? and "+
-			"((objects.visibility='VISIBILITY_TYPE_PUBLIC_READ') or (objects.visibility='VISIBILITY_TYPE_INHERIT' and buckets.visibility='VISIBILITY_TYPE_PUBLIC_READ'))",
-			startBlockNumber, endBlockNumber, true).
-		Limit(DeletedObjectsDefaultSize).
-		Order("objects.update_at, objects.object_id asc").
-		Find(&objects).Error
-	return objects, err
+
+	sort.Sort(ByUpdateAtAndID(totalObjects))
+
+	if len(totalObjects) > DeletedObjectsDefaultSize {
+		totalObjects = totalObjects[0:DeletedObjectsDefaultSize]
+	}
+	return totalObjects, err
 }
 
 // GetObjectByName get object info by an object name
@@ -100,14 +135,14 @@ func (b *BsDBImpl) GetObjectByName(objectName string, bucketName string, include
 	)
 
 	if includePrivate {
-		err = b.db.Table((&Object{}).TableName()).
+		err = b.db.Scopes(ReadObjectsTable(bucketName)).
 			Select("*").
 			Where("object_name = ? and bucket_name = ? and removed = false", objectName, bucketName).
 			Take(&object).Error
 		return object, err
 	}
 
-	err = b.db.Table((&Bucket{}).TableName()).
+	err = b.db.Scopes(ReadObjectsTable(bucketName)).
 		Select("objects.*").
 		Joins("left join objects on buckets.bucket_id = objects.bucket_id").
 		Where("objects.object_name = ? and objects.bucket_name = ? and objects.removed = false and "+
@@ -128,6 +163,10 @@ func (b *BsDBImpl) ListObjectsByObjectID(ids []common.Hash, includeRemoved bool)
 	if !includeRemoved {
 		filters = append(filters, RemovedFilter(includeRemoved))
 	}
+	//
+	//for idx, id := range ids {
+	//	getobjectbyid
+	//}
 
 	err = b.db.Table((&Object{}).TableName()).
 		Select("*").
@@ -135,4 +174,22 @@ func (b *BsDBImpl) ListObjectsByObjectID(ids []common.Hash, includeRemoved bool)
 		Scopes(filters...).
 		Find(&objects).Error
 	return objects, err
+}
+
+func GetObjectsTableName(bucketName string) string {
+	return GetObjectsTableNameByShardNumber(int(GetObjectsShardNumberByUID(bucketName)))
+}
+
+func GetObjectsShardNumberByUID(bucketName string) uint32 {
+	return murmur3.Sum32([]byte(bucketName)) % ObjectsNumberOfShards
+}
+
+func GetObjectsTableNameByShardNumber(shard int) string {
+	return fmt.Sprintf("objects_%02d", shard)
+}
+
+var ReadObjectsTable = func(uid string) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		return db.Table(GetObjectsTableName(uid))
+	}
 }
