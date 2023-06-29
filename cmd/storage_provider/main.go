f8f10dc71280f019e709f251f722b6f441ed751e
Barry <122767193+BarryTong65@users.noreply.github.com>
2023-06-29 11:20:49 +0800

    fix: fix billing info (#683)

--- cmd/storage_provider/main.go
+++ cmd/storage_provider/main.go
@@ -18,6 +18,7 @@ import (
 	"github.com/bnb-chain/greenfield-storage-provider/modular/executor"
 	"github.com/bnb-chain/greenfield-storage-provider/modular/gater"
 	"github.com/bnb-chain/greenfield-storage-provider/modular/manager"
+	"github.com/bnb-chain/greenfield-storage-provider/modular/metadata"
 	"github.com/bnb-chain/greenfield-storage-provider/modular/p2p"
 	"github.com/bnb-chain/greenfield-storage-provider/modular/receiver"
 	"github.com/bnb-chain/greenfield-storage-provider/modular/signer"
@@ -41,7 +42,7 @@ func registerModular() {
 	gfspapp.RegisterModular(module.P2PModularName, module.P2PModularDescription, p2p.NewP2PModular)
 	gfspapp.RegisterModular(module.ReceiveModularName, module.ReceiveModularDescription, receiver.NewReceiveModular)
 	gfspapp.RegisterModular(module.SignModularName, module.SignModularDescription, signer.NewSignModular)
-	//gfspapp.RegisterModular(metadata.MetadataModularName, metadata.MetadataModularDescription, metadata.NewMetadataModular)
+	gfspapp.RegisterModular(metadata.MetadataModularName, metadata.MetadataModularDescription, metadata.NewMetadataModular)
 	gfspapp.RegisterModular(module.UploadModularName, module.UploadModularDescription, uploader.NewUploadModular)
 	gfspapp.RegisterModular(blocksyncer.BlockSyncerModularName, blocksyncer.BlockSyncerModularDescription, blocksyncer.NewBlockSyncerModular)
 }
