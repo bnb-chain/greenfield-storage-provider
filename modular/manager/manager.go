8aa3318f52e84f900e7b2e1f841e95e79c390e04
joeylichang <joeycli0919@gmail.com>
2023-07-01 15:29:33 +0800

    refactor: manager dispatch task

--- modular/manager/manager.go
+++ modular/manager/manager.go
@@ -463,32 +463,33 @@ func (m *ManageModular) FilterUploadingTask(qTask task.Task) bool {
 	return false
 }
 
-func (m *ManageModular) PickUpTask(ctx context.Context, tasks []task.Task) task.Task {
+func (m *ManageModular) PickUpTask(ctx context.Context, tasks []task.Task) (task.Task, []task.Task) {
 	if len(tasks) == 0 {
-		return nil
+		return nil, nil
 	}
 	if len(tasks) == 1 {
 		log.CtxDebugw(ctx, "only one task for picking")
-		return tasks[0]
+		return tasks[0], nil
 	}
 	sort.Slice(tasks, func(i, j int) bool {
 		return tasks[i].GetPriority() < tasks[j].GetPriority()
 	})
 	var totalPriority int
-	for _, task := range tasks {
-		totalPriority += int(task.GetPriority())
+	for _, t := range tasks {
+		totalPriority += int(t.GetPriority())
 	}
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	randPriority := r.Intn(totalPriority)
-	totalPriority = 0
+	log.Debugw("pick up task", "total_priority", totalPriority, "rand_priority", randPriority)
 
-	for _, task := range tasks {
-		totalPriority += int(task.GetPriority())
+	totalPriority = 0
+	for i, t := range tasks {
+		totalPriority += int(t.GetPriority())
 		if totalPriority >= randPriority {
-			return task
+			return t, append(tasks[:i], tasks[i+1:]...)
 		}
 	}
-	return nil
+	return nil, tasks
 }
 
 func (m *ManageModular) syncConsensusInfo(ctx context.Context) {
