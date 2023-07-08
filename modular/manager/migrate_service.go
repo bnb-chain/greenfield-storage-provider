ce5e808a44c8076129312ef6b2bd09e8281ae396
will-2012 <xibaow2020@qq.com>
2023-07-08 12:18:06 +0800

    feat: refactor sp exit scheduler

--- modular/manager/migrate_service.go
+++ modular/manager/migrate_service.go
@@ -21,18 +21,9 @@ func (m *ManageModular) getSPID() (uint32, error) {
 // NotifyMigrateSwapOut is used to receive migrate gvg task from src sp.
 func (m *ManageModular) NotifyMigrateSwapOut(ctx context.Context, swapOut *virtualgrouptypes.MsgSwapOut) error {
 	if m.spExitScheduler == nil {
-		return ErrNotifyMigrateGVG
+		return ErrNotifyMigrateSwapOut
 	}
 
-	//return m.spExitScheduler.AddNewMigrateGVGUnit(&GlobalVirtualGroupMigrateExecuteUnit{
-	//	gvg:             task.GetGvg(),
-	//	redundancyIndex: task.GetRedundancyIdx(),
-	//	isRemoted:       true, // from src sp
-	//	isConflicted:    false,
-	//	isSecondary:     false,
-	//	srcSP:           task.GetSrcSp(),
-	//	destSP:          task.GetDestSp(),
-	//	migrateStatus:   WaitForMigrate,
-	//})
-	return nil
+	// TODO: check SuccessorSpId
+	return m.spExitScheduler.AddSwapOutToTaskRunner(swapOut)
 }
