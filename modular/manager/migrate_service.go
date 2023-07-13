9ca4edb255fd0ce8d8a2c090bc8b1fc943e8015d
BarryTong65 <barrytong.work@gmail.com>
2023-07-13 15:03:19 +0800

    fix: adapt sp id ListExpiredBucketsBySp

--- modular/manager/migrate_service.go
+++ modular/manager/migrate_service.go
@@ -6,6 +6,18 @@ import (
 	"github.com/bnb-chain/greenfield-storage-provider/core/task"
 )
 
+func (m *ManageModular) getSPID() (uint32, error) {
+	if m.spID != 0 {
+		return m.spID, nil
+	}
+	spInfo, err := m.baseApp.Consensus().QuerySP(context.Background(), m.baseApp.OperatorAddress())
+	if err != nil {
+		return 0, err
+	}
+	m.spID = spInfo.GetId()
+	return m.spID, nil
+}
+
 // NotifyMigrateGVG is used to receive migrate gvg task from src sp.
 func (m *ManageModular) NotifyMigrateGVG(ctx context.Context, task task.MigrateGVGTask) error {
 	if m.spExitScheduler == nil {
