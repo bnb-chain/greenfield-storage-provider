42dfa75a93cb74cf3975254207291115beaf6053
will-2012 <117156346+will-2012@users.noreply.github.com>
2023-07-24 20:02:07 +0800

    feat: add generate gvg sp policy (#890)

--- base/gfspvgmgr/virtual_group_manager.go
+++ base/gfspvgmgr/virtual_group_manager.go
@@ -15,7 +15,6 @@ import (
 	"github.com/bnb-chain/greenfield-storage-provider/pkg/log"
 	"github.com/bnb-chain/greenfield-storage-provider/util"
 	sptypes "github.com/bnb-chain/greenfield/x/sp/types"
-	storagetypes "github.com/bnb-chain/greenfield/x/storage/types"
 	virtualgrouptypes "github.com/bnb-chain/greenfield/x/virtualgroup/types"
 )
 
@@ -158,23 +157,16 @@ type spManager struct {
 	otherSPs []*sptypes.StorageProvider
 }
 
-func (sm *spManager) generateVirtualGroupMeta(param *storagetypes.Params) (*vgmgr.GlobalVirtualGroupMeta, error) {
-	secondarySPNumber := int(param.GetRedundantDataChunkNum() + param.GetRedundantParityChunkNum())
-	if sm.selfSP == nil || len(sm.otherSPs) < secondarySPNumber {
-		return nil, fmt.Errorf("no enough sp")
-	}
-	secondarySPIDs := make([]uint32, 0)
+func (sm *spManager) generateVirtualGroupMeta(genPolicy vgmgr.GenerateGVGSecondarySPsPolicy) (*vgmgr.GlobalVirtualGroupMeta, error) {
 	for _, sp := range sm.otherSPs {
 		if !sp.IsInService() {
 			continue
 		}
-		secondarySPIDs = append(secondarySPIDs, sp.GetId())
-		if len(secondarySPIDs) == secondarySPNumber {
-			break
-		}
+		genPolicy.AddCandidateSP(sp.GetId())
 	}
-	if len(secondarySPIDs) < secondarySPNumber {
-		return nil, fmt.Errorf("no enough sp")
+	secondarySPIDs, err := genPolicy.GenerateGVGSecondarySPs()
+	if err != nil {
+		return nil, err
 	}
 	return &vgmgr.GlobalVirtualGroupMeta{
 		PrimarySPID:        sm.selfSP.Id,
@@ -385,10 +377,10 @@ func (vgm *virtualGroupManager) ForceRefreshMeta() error {
 }
 
 // GenerateGlobalVirtualGroupMeta is used to generate a gvg meta.
-func (vgm *virtualGroupManager) GenerateGlobalVirtualGroupMeta(param *storagetypes.Params) (*vgmgr.GlobalVirtualGroupMeta, error) {
+func (vgm *virtualGroupManager) GenerateGlobalVirtualGroupMeta(genPolicy vgmgr.GenerateGVGSecondarySPsPolicy) (*vgmgr.GlobalVirtualGroupMeta, error) {
 	vgm.mutex.RLock()
 	defer vgm.mutex.RUnlock()
-	return vgm.spManager.generateVirtualGroupMeta(param)
+	return vgm.spManager.generateVirtualGroupMeta(genPolicy)
 }
 
 // PickSPByFilter is used to pick sp by filter check.
