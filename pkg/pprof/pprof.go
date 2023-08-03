6cf70df61d763f6f5d541505e5e39ffda0dc4b0f
j75689 <j75689@gmail.com>
2023-08-03 16:16:04 +0800

    fix: add missing path for pprof server

--- pkg/pprof/pprof.go
+++ pkg/pprof/pprof.go
@@ -82,4 +82,13 @@ func (p *PProf) registerProfiler(r *mux.Router) {
 	r.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
 	r.HandleFunc("/debug/pprof/trace", pprof.Trace)
 	r.Handle("/debug/fgprof", fgprof.Handler())
+
+	// Manually add support for paths linked to by index page at /debug/pprof/
+	r.Handle("/debug/pprof/goroutine", pprof.Handler("goroutine"))
+	r.Handle("/debug/pprof/heap", pprof.Handler("heap"))
+	r.Handle("/debug/pprof/allocs", pprof.Handler("allocs"))
+	r.Handle("/debug/pprof/threadcreate", pprof.Handler("threadcreate"))
+	r.Handle("/debug/pprof/block", pprof.Handler("block"))
+	r.Handle("/debug/pprof/mutex", pprof.Handler("mutex"))
+
 }
