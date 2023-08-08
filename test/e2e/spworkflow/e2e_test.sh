bb93c8be613440143c4ec71592eb6e8b0f7b113f
ruojunm@gmail.com <clyde.m@nodereal.io>
2023-08-08 17:17:53 +0800

    fix: fix e2e

--- test/e2e/spworkflow/e2e_test.sh
+++ test/e2e/spworkflow/e2e_test.sh
@@ -132,7 +132,7 @@ function test_file_size_less_than_16_mb() {
   set -e
   cd ${workspace}/greenfield-cmd/build/
   ./gnfd-cmd -c ./config.toml --home ./ --passwordfile password.txt object put --contentType "application/json" ${workspace}/test/e2e/spworkflow/testdata/example.json gnfd://${BUCKET_NAME}
-  sleep 16
+  sleep 32
   ./gnfd-cmd -c ./config.toml --home ./ --passwordfile password.txt object get gnfd://${BUCKET_NAME}/example.json ./test_data.json
   check_md5 ${workspace}/test/e2e/spworkflow/testdata/example.json ./test_data.json
   cat test_data.json
@@ -146,7 +146,7 @@ function test_file_size_greater_than_16_mb() {
   cd ${workspace}/greenfield-cmd/build/
   dd if=/dev/urandom of=./random_file bs=17M count=1
   ./gnfd-cmd -c ./config.toml --home ./ --passwordfile password.txt object put --contentType "application/octet-stream" ./random_file gnfd://${BUCKET_NAME}/random_file
-  sleep 16
+  sleep 32
   ./gnfd-cmd -c ./config.toml --home ./ --passwordfile password.txt object get gnfd://${BUCKET_NAME}/random_file ./new_random_file
   sleep 10
   check_md5 ./random_file ./new_random_file
