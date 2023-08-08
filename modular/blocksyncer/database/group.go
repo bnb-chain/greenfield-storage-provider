f245f5f801b87eacb694c958242690fa1701a141
VM <112189277+sysvm@users.noreply.github.com>
2023-08-08 21:11:15 +0800

    ci: add commit lint ci yaml (#990)
    Co-authored-by: VM <arimas@foxmail.com>

--- modular/blocksyncer/database/group.go
+++ modular/blocksyncer/database/group.go
@@ -2,10 +2,11 @@ package database
 
 import (
 	"context"
-
-	"github.com/forbole/juno/v4/models"
+	"fmt"
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
+
+	"github.com/forbole/juno/v4/models"
 )
 
 func (db *DB) CreateGroup(ctx context.Context, groupMembers []*models.Group) error {
@@ -27,4 +28,19 @@ func (db *DB) CreateGroupToSQL(ctx context.Context, groupMembers []*models.Group
 func (db *DB) UpdateGroupToSQL(ctx context.Context, group *models.Group) (string, []interface{}) {
 	stat := db.Db.Session(&gorm.Session{DryRun: true}).Table((&models.Group{}).TableName()).Where("group_id = ? AND account_id = ?", group.GroupID, group.AccountID).Updates(group).Statement
 	return stat.SQL.String(), stat.Vars
+}
+
+func (db *DB) RenewGroupMemberToSQL(ctx context.Context, groups []*models.Group) (string, []interface{}) {
+	var caseStatement string
+	for _, g := range groups {
+		caseStatement += fmt.Sprintf(
+			"WHEN (field1 = '%v' AND field2 = '%v') THEN '%d' ",
+			g.GroupID, g.AccountID, g.ExpirationTime)
+	}
+	sql := fmt.Sprintf("UPDATE groups SET expiration_time = CASE %s END", caseStatement)
+	//err := db.Db.Exec(sql).Error
+	//if err != nil {
+	//	log.Infow("failed to renew member err", "error", err)
+	//}
+	return sql, nil
 }
