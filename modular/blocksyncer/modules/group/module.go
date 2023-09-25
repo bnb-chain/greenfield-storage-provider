d2e55758f80f0559bdfabc58bf1159b8341996e8
constwz <changbohao30@gmail.com>
2023-09-25 11:10:27 +0800

    fix:delete group fix job

--- modular/blocksyncer/modules/group/module.go
+++ modular/blocksyncer/modules/group/module.go
@@ -2,14 +2,12 @@ package group
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/forbole/juno/v4/models"
 	"github.com/forbole/juno/v4/modules"
 	"gorm.io/gorm/schema"
 
 	"github.com/bnb-chain/greenfield-storage-provider/modular/blocksyncer/database"
-	"github.com/bnb-chain/greenfield-storage-provider/pkg/log"
 )
 
 const (
@@ -43,47 +41,7 @@ func (m *Module) PrepareTables() error {
 	return m.db.PrepareTables(context.TODO(), []schema.Tabler{&models.Group{}})
 }
 
-// In this version, we need to create a unique index for the groups table
-// Before creating, we need to remove duplicate data from the table
-// This code will be removed in the next version
-func (m *Module) temporaryFixGroupData() {
-	log.Infof("start fix groups data")
-	exist := m.db.Db.Migrator().HasTable("groups")
-	if !exist {
-		log.Infof("fix groups data success")
-		return
-	}
-	tx := m.db.Db.Begin()
-	err := tx.Exec("CREATE TABLE `groups_temp` LIKE `groups`").Error
-	if err != nil {
-		tx.Rollback()
-		panic(fmt.Sprintf("fix groups data failed err: %v", err))
-	}
-	err = tx.Exec("INSERT INTO `groups_temp` select c1.* FROM `groups` c1 INNER JOIN `groups` c2 WHERE c1.id < c2.id AND c1.account_id = c2.account_id and c1.group_id = c2.group_id order by c1.id").Error
-	if err != nil {
-		tx.Rollback()
-		panic(fmt.Sprintf("fix groups data failed err: %v", err))
-	}
-	err = tx.Exec("delete from `groups` where id in (select id from `groups_temp`)").Error
-	if err != nil {
-		tx.Rollback()
-		panic(fmt.Sprintf("fix groups data failed err: %v", err))
-	}
-	err = tx.Exec("DROP TABLE `groups_temp`").Error
-	if err != nil {
-		tx.Rollback()
-		panic(fmt.Sprintf("fix groups data failed err: %v", err))
-	}
-	err = tx.Commit().Error
-	if err != nil {
-		tx.Rollback()
-		panic(fmt.Sprintf("fix groups data failed err: %v", err))
-	}
-	log.Infof("fix groups data success")
-}
-
 // AutoMigrate implements
 func (m *Module) AutoMigrate() error {
-	m.temporaryFixGroupData()
 	return m.db.AutoMigrate(context.TODO(), []schema.Tabler{&models.Group{}})
 }
