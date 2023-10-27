b7296b77180b77efafdd739ce805df2869cb358d
annielz <lmlmzhen@sina.com>
2023-10-27 13:35:11 +0800

    feat: change info to user from log to fmt.printf

--- cmd/command/recovery.go
+++ cmd/command/recovery.go
@@ -104,9 +104,9 @@ func recoverObjectAction(ctx *cli.Context) error {
 	for range taskCheckTicker.C {
 		stats, _ := client.GetTasksStats(context.Background())
 		if stats.GetRecoveryProcessCount() > 0 {
-			log.Infof("Processing recovery tasks, update every 5 seconds. Waiting Tasks Number: %d. Recovered: %s. Failed: %s", stats.GetRecoveryProcessCount(), stats.GetRecoverySucceedList(), stats.GetRecoveryFailedList())
+			fmt.Printf("Processing recovery tasks, update every 5 seconds. \n Waiting Tasks Number: %d \n Recovered: %s \n Failed: %s\n\n", stats.GetRecoveryProcessCount(), stats.GetRecoverySucceedList(), stats.GetRecoveryFailedList())
 		} else {
-			log.Infof("Finished process all recovery objects task on background. Recovered: %s. Failed: %s \n", stats.GetRecoverySucceedList(), stats.GetRecoveryFailedList())
+			fmt.Printf("Finished process all recovery objects task on background. \n Recovered: %s \n Failed: %s \n", stats.GetRecoverySucceedList(), stats.GetRecoveryFailedList())
 			break
 		}
 	}
