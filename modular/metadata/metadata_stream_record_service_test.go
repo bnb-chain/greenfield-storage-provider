5ac476a3297b1eeb24f5fe94fd4f47ef1519f114
Ruojun Meng <ruojunm@gmail.com>
2023-11-01 18:19:32 +0800

    feat: add gnfd cmd to query sq incomes -- add uts

---
+++ modular/metadata/metadata_stream_record_service_test.go
@@ -0,0 +1,117 @@
+package metadata
+
+import (
+	"context"
+	"cosmossdk.io/math"
+	"github.com/bnb-chain/greenfield-storage-provider/modular/metadata/types"
+	"github.com/bnb-chain/greenfield-storage-provider/store/bsdb"
+	"github.com/forbole/juno/v4/common"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/mock/gomock"
+	"math/big"
+	"testing"
+)
+
+func TestGfSpPrimarySpIncomeDetails_Success(t *testing.T) {
+	a := setup(t)
+	ctrl := gomock.NewController(t)
+	m := bsdb.NewMockBSDB(ctrl)
+	a.baseApp.SetGfBsDB(m)
+	big, _ := new(big.Int).SetString("123456789012345678901234567890", 10)
+	m.EXPECT().GetPrimarySPStreamRecordBySpID(gomock.Any()).DoAndReturn(
+		func(spID any) ([]*bsdb.PrimarySpIncomeMeta, error) {
+			return []*bsdb.PrimarySpIncomeMeta{&bsdb.PrimarySpIncomeMeta{
+				GlobalVirtualGroupFamilyId: 48,
+				StreamRecord: bsdb.StreamRecord{
+					ID:                0,
+					Account:           common.HexToAddress("0x11E0A11A7A01E2E757447B52FBD7152004AC699D"),
+					CrudTimestamp:     0,
+					NetflowRate:       (*common.Big)(big),
+					StaticBalance:     (*common.Big)(big),
+					BufferBalance:     (*common.Big)(big),
+					LockBalance:       (*common.Big)(big),
+					Status:            "STREAM_ACCOUNT_STATUS_ACTIVE",
+					SettleTimestamp:   0,
+					OutFlowCount:      0,
+					FrozenNetflowRate: (*common.Big)(big),
+				},
+			}}, nil
+		},
+	).Times(1)
+	a.baseApp.SetGfBsDB(m)
+	resp, err := a.GfSpPrimarySpIncomeDetails(context.Background(), &types.GfSpPrimarySpIncomeDetailsRequest{
+		SpId: 1,
+	})
+	assert.Nil(t, err)
+	assert.Equal(t, uint32(48), resp.PrimarySpIncomeDetails[0].VgfId)
+	assert.Equal(t, math.NewIntFromBigInt((*common.Big)(big).Raw()), resp.PrimarySpIncomeDetails[0].StreamRecord.NetflowRate)
+}
+
+func TestGfSpPrimarySpIncomeDetails_Failed(t *testing.T) {
+	a := setup(t)
+	ctrl := gomock.NewController(t)
+	m := bsdb.NewMockBSDB(ctrl)
+	a.baseApp.SetGfBsDB(m)
+	m.EXPECT().GetPrimarySPStreamRecordBySpID(gomock.Any()).DoAndReturn(
+		func(spID any) ([]*bsdb.PrimarySpIncomeMeta, error) {
+			return nil, ErrExceedRequest
+		},
+	).Times(1)
+	a.baseApp.SetGfBsDB(m)
+	_, err := a.GfSpPrimarySpIncomeDetails(context.Background(), &types.GfSpPrimarySpIncomeDetailsRequest{
+		SpId: 1,
+	})
+	assert.NotNil(t, err)
+}
+
+func TestGfSpSecondarySpIncomeDetails_Success(t *testing.T) {
+	a := setup(t)
+	ctrl := gomock.NewController(t)
+	m := bsdb.NewMockBSDB(ctrl)
+	a.baseApp.SetGfBsDB(m)
+	big, _ := new(big.Int).SetString("123456789012345678901234567890", 10)
+	m.EXPECT().GetSecondarySPStreamRecordBySpID(gomock.Any()).DoAndReturn(
+		func(spID any) ([]*bsdb.SecondarySpIncomeMeta, error) {
+			return []*bsdb.SecondarySpIncomeMeta{&bsdb.SecondarySpIncomeMeta{
+				GlobalVirtualGroupId: 4,
+				StreamRecord: bsdb.StreamRecord{
+					ID:                0,
+					Account:           common.HexToAddress("0x11E0A11A7A01E2E757447B52FBD7152004AC699D"),
+					CrudTimestamp:     0,
+					NetflowRate:       (*common.Big)(big),
+					StaticBalance:     (*common.Big)(big),
+					BufferBalance:     (*common.Big)(big),
+					LockBalance:       (*common.Big)(big),
+					Status:            "STREAM_ACCOUNT_STATUS_ACTIVE",
+					SettleTimestamp:   0,
+					OutFlowCount:      0,
+					FrozenNetflowRate: (*common.Big)(big),
+				},
+			}}, nil
+		},
+	).Times(1)
+	a.baseApp.SetGfBsDB(m)
+	resp, err := a.GfSpSecondarySpIncomeDetails(context.Background(), &types.GfSpSecondarySpIncomeDetailsRequest{
+		SpId: 1,
+	})
+	assert.Nil(t, err)
+	assert.Equal(t, uint32(4), resp.SecondarySpIncomeDetails[0].GvgId)
+	assert.Equal(t, math.NewIntFromBigInt((*common.Big)(big).Raw()), resp.SecondarySpIncomeDetails[0].StreamRecord.NetflowRate)
+}
+
+func TestGfSpSecondarySpIncomeDetails_Failed(t *testing.T) {
+	a := setup(t)
+	ctrl := gomock.NewController(t)
+	m := bsdb.NewMockBSDB(ctrl)
+	a.baseApp.SetGfBsDB(m)
+	m.EXPECT().GetSecondarySPStreamRecordBySpID(gomock.Any()).DoAndReturn(
+		func(spID any) ([]*bsdb.SecondarySpIncomeMeta, error) {
+			return nil, ErrExceedRequest
+		},
+	).Times(1)
+	a.baseApp.SetGfBsDB(m)
+	_, err := a.GfSpSecondarySpIncomeDetails(context.Background(), &types.GfSpSecondarySpIncomeDetailsRequest{
+		SpId: 1,
+	})
+	assert.NotNil(t, err)
+}
--- store/bsdb/stream_record_test.go
+++ store/bsdb/stream_record_test.go
@@ -1,1 +1,40 @@
 package bsdb
+
+import (
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+const (
+	mockQueryPrimarySPStreamRecord   = "SELECT s.* , g.global_virtual_group_family_id FROM `stream_records` s join `global_virtual_group_families` g on g.virtual_payment_address = s.account where g.primary_sp_id=? and g.removed = false"
+	mockQuerySecondarySPStreamRecord = "SELECT s.* , g.global_virtual_group_id FROM `stream_records` s join `global_virtual_groups` g on g.virtual_payment_address = s.account where FIND_IN_SET(?, g.secondary_sp_ids) > 0 and removed = false;"
+)
+
+func TestBsDBImpl_GetPrimarySPStreamRecordBySpID(t *testing.T) {
+	s, mock := setupDB(t)
+	mock.ExpectQuery(mockQueryPrimarySPStreamRecord).
+		WithArgs(1).
+		WillReturnRows(sqlmock.NewRows([]string{
+			"id",
+		}).
+			AddRow(29265))
+
+	streamRecords, err := s.GetPrimarySPStreamRecordBySpID(1)
+	assert.Nil(t, err)
+	assert.NotNil(t, streamRecords)
+}
+
+func TestBsDBImpl_GetSecondarySPStreamRecordBySpID(t *testing.T) {
+	s, mock := setupDB(t)
+	mock.ExpectQuery(mockQuerySecondarySPStreamRecord).
+		WithArgs(1).
+		WillReturnRows(sqlmock.NewRows([]string{
+			"id",
+		}).
+			AddRow(29265))
+
+	streamRecords, err := s.GetSecondarySPStreamRecordBySpID(1)
+	assert.Nil(t, err)
+	assert.NotNil(t, streamRecords)
+}
