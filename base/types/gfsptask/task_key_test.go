07032e4c781239f74e7c645bb4c4fdb468d2046a
Alexxxxxx <118710506+alexgao001@users.noreply.github.com>
2024-02-19 14:27:10 +0800

    fix: exclude timestamp from recovery piece key to avoid duplicate requests (#1344)

--- base/types/gfsptask/task_key_test.go
+++ base/types/gfsptask/task_key_test.go
@@ -3,5 +3,5 @@ package gfsptask
 import "testing"
 
 func TestGfSpRecoverPieceTaskKey(t *testing.T) {
-	GfSpRecoverPieceTaskKey("1", "2", "3", 1, -1, 1)
+	GfSpRecoverPieceTaskKey("1", "2", "3", 1, -1)
 }
